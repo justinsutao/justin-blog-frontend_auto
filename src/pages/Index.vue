b2c87d0e63ff8078c5d47b5a2aaadb83e8cd8e2e
justinsutao <525878612@qq.com>
2021-03-09 15:44:26 +0000

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,113 @@
+<template>
+  <Layout>
+    <!-- Page Header -->
+    <header class="masthead"
+            :style="{backgroundImage: `url(${GRIDSOME_API_URL}${base.cover.url})`}">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="site-heading">
+              <h1>{{base.title}}</h1>
+              <span class="subheading">{{base.subTitle}}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <!-- Main Content -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="post-preview" v-for="edge in $page.posts.edges" :key="edge.node.id">
+            <g-link :to="'/post/' + edge.node.id">
+              <h2 class="post-title">
+                {{edge.node.title}}
+              </h2>
+              <!-- <h3 class="post-subtitle">
+                Problems look mighty small from 150 miles up
+              </h3> -->
+            </g-link>
+            <p class="post-meta">Posted by
+              <a href="#">Start Bootstrap</a>
+              on {{edge.node.created_at}}
+            </p>
+            <p>
+              <span v-for="tag in edge.node.tags" :key="tag.id">
+                <g-link :to="'/tag/' + tag.id">{{tag.title}}</g-link>
+                &nbsp;&nbsp;
+              </span>
+            </p>
+            <hr>
+          </div>
+          
+          <hr>
+          <!-- Pager -->
+          <!-- <div class="clearfix">
+            <a class="btn btn-primary float-right"
+               href="#">Older Posts &rarr;</a>
+          </div> -->
+          <Pager :info="$page.posts.pageInfo" class="pager"></Pager>
+        </div>
+      </div>
+    </div>
+    <hr>
+  </Layout>
+</template>
+<page-query>
+query($page: Int){
+  posts: allStrapiPost(perPage:2, page:$page) @paginate{
+    pageInfo{
+      totalPages
+      currentPage
+    }
+    edges{
+      node{
+        title
+        id
+        created_at
+        tags{
+          title
+          id
+        }
+      }
+    }
+  }
+  base: allStrapiBase{
+    edges{
+      node{
+        id
+        title
+        subTitle
+        cover{
+          url
+        }
+      }
+    }
+  }
+}
+</page-query>
+<script>
+import {Pager} from 'gridsome'
+export default {
+  components:{
+    Pager
+  },
+  computed:{
+    base(){
+      return this.$page.base.edges[0].node
+    }
+  },
+  metaInfo: {
+    title: "Hello, world!",
+  },
+};
+</script>
+
+<style>
+.pager a{
+  width:20px;
+  text-align: center;
+  display: inline-block;
+}
+</style>
---
+++ src/templates/Post.vue
@@ -0,0 +1,70 @@
+<!-- 组件说明 -->
+<template>
+  <layout>
+    <!-- Page Header -->
+    <header class="masthead"
+            :style="{
+              backgroundImage:$page.post.cover?`url(${GRIDSOME_API_URL}${$page.post.cover.url})`:`url(/img/post-bg.jpg)`
+            }">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="post-heading">
+              <h1>{{$page.post.title}}</h1>
+              <!-- <h2 class="subheading">Problems look mighty small from 150 miles up</h2> -->
+              <span class="meta">Posted by
+                <a href="#">Start Bootstrap</a>
+                on {{$page.post.created_at}}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Post Content -->
+    <article>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto" v-html="mdToHTML($page.post.content)"></div>
+        </div>
+      </div>
+    </article>
+
+    <hr>
+  </layout>
+</template>
+
+<page-query>
+query($id:ID!){
+  post: strapiPost(id:$id){
+     id
+    title
+    created_at
+    content
+    cover{
+      url
+    }
+    tags{
+      id
+      title
+    }
+  }
+}
+</page-query>
+
+<script>
+import markdownIt from 'markdown-it'
+const md = new markdownIt()
+export default {
+  name: "postPage",
+  methods: {
+    mdToHTML(markdown){
+      return md.render(markdown)
+    }
+  }
+};
+</script>
+
+<style>
+</style>
